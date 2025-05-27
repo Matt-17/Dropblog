ccf77e601dc7c789e5dbcda78e3991d9e2b5a08d
Matthias Voigt <mv@outlook.de>
2025-05-27 20:56:52 +0200

    sss

--- src/Views/Components/PostCard.php
+++ src/Views/Components/PostCard.php
@@ -4,7 +4,6 @@
  */
 ?>
 <article class="post-item">
-    <h2><a href="/post/<?= $post->id ?>"><?= htmlspecialchars($post->title) ?></a></h2>
     <div class="post-meta">
         <time datetime="<?= $post->date ?>"><?= $post->date ?></time>
     </div>
