d8ee74727ec77d33b92d279fcd9b79f9d6a17534
Matthias Voigt <mv@outlook.de>
2025-06-02 15:33:23 +0200

    fdg

--- web/src/Migrations/004_add_new_post_types_to_check.sql
+++ web/src/Migrations/004_add_new_post_types_to_check.sql
@@ -1,15 +1,9 @@
 -- Add new post types to the CHECK constraint on the 'type' column in the posts table.
--- This script attempts to drop the existing CHECK constraint and add a new one
--- with the expanded list of allowed types.
--- Note: The constraint name might vary depending on your MySQL version or tools used.
--- If this script fails due to a constraint name error, you may need to
--- manually find the constraint name and update this script.
+-- This script attempts to modify the column definition to include the updated CHECK constraint.
+-- This approach might be more compatible if dropping the constraint by name is problematic.
 
 ALTER TABLE posts
-DROP CHECK posts_chk_1; -- Replace 'posts_chk_1' with the actual constraint name if different
-
-ALTER TABLE posts
-ADD CONSTRAINT posts_chk_1 CHECK (type IN (
+MODIFY COLUMN type VARCHAR(20) NOT NULL DEFAULT 'note' CHECK (type IN (
     'note', 'link', 'comment', 'quote', 'photo', 'code', 'question',
     'shopping', 'rant', 'poll', 'media', 'book', 'announcement', 'calendar'
 )); 
