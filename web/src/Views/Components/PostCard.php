119f0cde6dbb6e74cc8ff1b8ea6ec2039971ecbd
Matthias Voigt <mv@outlook.de>
2025-06-02 16:58:08 +0200

    ARGGHH

--- web/src/Views/Components/PostCard.php
+++ web/src/Views/Components/PostCard.php
@@ -6,16 +6,6 @@
 use Dropblog\Models\Post;
 use Dropblog\Models\PostModel;
 use Dropblog\Utils\HashIdHelper;
-
-function highlight_keywords($text, $keywords) {
-    if (!$keywords || !is_array($keywords)) return $text;
-    foreach ($keywords as $word) {
-        if (trim($word) !== '') {
-            $text = preg_replace('/(' . preg_quote($word, '/') . ')/iu', '<mark>$1</mark>', $text);
-        }
-    }
-    return $text;
-}
 ?>
 
 <article class="post">
