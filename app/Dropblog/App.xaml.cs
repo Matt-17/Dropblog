66217819401fb623e492ab2f3011377949e92425
Matthias Voigt <mv@outlook.de>
2025-06-02 14:18:51 +0200

    Added MAUI app

---
+++ app/Dropblog/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Dropblog
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "Dropblog" };
+        }
+    }
+}
